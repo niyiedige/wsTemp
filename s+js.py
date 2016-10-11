35dedc1d2be264aa27ce3a22ad9c4606ac55f1d8
niyiedige <wangxc3959@outlook.com>
2016-10-11 11:33:48 -0400

    Add files via upload

---
+++ s+js.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from lxml import html
+#need to include phantomjs in the PATH
+
+link = 'https://l3com.taleo.net/careersection/l3_ext_us/jobsearch.ftl'
+
+driver1 = webdriver.PhantomJS()
+driver1.get('http://career.cmbchina.com/Campus/Position.aspx?id=10231')
+driver1.set_window_size(1120, 550)
+s = BeautifulSoup(driver1.page_source,"lxml")
+b=html.fromstring(driver1.page_source)
+print(b.xpath('//*[@id="rightdiv"]/div/div[1]/text()'))
+
+
+
+driver1.quit()
+
+
+
